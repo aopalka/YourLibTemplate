153ae13d45eb7ae0303960890e3fd6b126e47f1c
aopalka <79727669+aopalka@users.noreply.github.com>
2024-06-16 12:52:11 -0400

    Initial Commit Template Project 0.0.0.1

---
+++ src/main/java/YourLibTemplate/YourLibTemplate.java
@@ -0,0 +1,14 @@
+package YourLibTemplate;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+/**
+ * Your Library Class
+ */
+public class YourLibTemplate {
+  /**
+   * Constructor for YourLibTemplate
+   */
+  public YourLibTemplate() {
+    SmartDashboard.putString("Library", "YourLibTemplate");
+  }
+}
