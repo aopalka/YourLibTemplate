5f57419d468044aad5b03333bfdbcb6d7650e3d6
aopalka <79727669+aopalka@users.noreply.github.com>
2024-06-16 13:02:08 -0400

    Testing

--- src/main/java/YourLibTemplate/YourLibTemplate.java
+++ src/main/java/YourLibTemplate/YourLibTemplate.java
@@ -1,13 +1,10 @@
 package YourLibTemplate;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-/**
- * Your Library Class
- */
+
+/** Your Library Class */
 public class YourLibTemplate {
-  /**
-   * Constructor for YourLibTemplate
-   */
+  /** Constructor for YourLibTemplate */
   public YourLibTemplate() {
     SmartDashboard.putString("Library", "YourLibTemplate");
   }
